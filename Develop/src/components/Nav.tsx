6df6c9b65e9cc6601e2fc04a959bd89ede813eea
gamalmubarak <eng-gemy@hotmail.com>
2025-04-04 15:05:06 -0400

    code updated on nav.tsx

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,34 @@
+import { NavLink, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const location = useLocation();
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink
+            to="/"
+            className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/SavedCandidates"
+            className={
+              location.pathname === '/SavedCandidates'
+                ? 'nav-link active'
+                : 'nav-link'
+            }
+          >
+            Potential Candidates
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
